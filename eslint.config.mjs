3fe2228c0e12cf1f01a8548cd58924bdd3eb135d
AlvesKZ <igoralvex01@gmail.com>
2025-06-03 18:32:54 -0300

    Cria componente principal

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
-    jest: true,
+    es2024: true,
     node: true,
+    jest: true,
   },
   extends: [
     'eslint:recommended',
@@ -11,25 +12,27 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parser: '@babel/eslint-parser',
   parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-react'],
+    },
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', 'react-hooks'],
+  plugins: ['react', 'react-hooks', 'prettier'],
   settings: {
     react: {
       version: 'detect',
     },
   },
   rules: {
+    'prettier/prettier': 'warn',
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
   },
 };
